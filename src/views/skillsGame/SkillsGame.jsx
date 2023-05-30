56157b5375492aa29cbe644d9eef7656329bd808
Alan.-322 <99999496+jon0010@users.noreply.github.com>
2023-05-30 20:57:55 -0300

    portfolio finalizado, juegoskills en desarrollo

--- src/views/skillsGame/SkillsGame.jsx
+++ src/views/skillsGame/SkillsGame.jsx
@@ -3,6 +3,7 @@ import "./SkillsGame.css";
 import suelo from "../../assets/suelo.png";
 import cielo from "../../assets/cielo.jpg";
 import brick1 from "../../assets/brickMario1.gif";
+import goomba from "../../assets/goomba.jpg";
 import spriteRunLeft from "../../assets/spriteRunLeft.png";
 import spriteRunright from "../../assets/spriteRunRight.png";
 import spriteStandLeft from "../../assets/spriteStandLeft.png";
@@ -90,6 +91,32 @@ const SkillsGame = () => {
       }
     }
 
+    class Enemy {
+      constructor(x, y, image) {
+        this.position = {
+          x,
+          y,
+        };
+        this.image = image;
+        this.width = image.width;
+        this.height = image.height;
+        this.velocity = {
+          x: -2, // Velocidad en el eje X para el movimiento de izquierda a derecha
+          y: 0,
+        };
+      }
+
+      draw() {
+        context.drawImage(this.image, this.position.x, this.position.y);
+      }
+
+      update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.draw();
+      }
+    }
+
     class Platform {
       constructor({ x, y, image }) {
         this.position = {
@@ -130,10 +157,15 @@ const SkillsGame = () => {
 
     let sueloImage = createImage(suelo);
     let brick1Image = createImage(brick1);
+    let cieloImage = createImage(cielo);
+    let goombaImage = createImage(goomba);
 
     let player = new Player();
     let platforms = [];
     let genericObjects = [];
+    let enemies = [];
+
+    let currentKey;
 
     const keys = {
       right: {
@@ -149,46 +181,170 @@ const SkillsGame = () => {
     const init = () => {
       sueloImage = createImage(suelo);
       brick1Image = createImage(brick1);
+      cieloImage = createImage(cielo);
+      goombaImage = createImage(goomba);
 
       player = new Player();
       platforms = [
         new Platform({
-          x:
-            sueloImage.width * 4 +
-            700 -
-            2 +
-            sueloImage.width -
-            brick1Image.width,
-          y: 206,
-          image: createImage(brick1),
+          x: 0,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+          cieloImage,
         }),
-        new Platform({ x: 0, y: 426, image: sueloImage }),
-        new Platform({ x: sueloImage.width, y: 426, image: sueloImage }),
         new Platform({
-          x: sueloImage.width * 2 + 100,
-          y: 426,
+          x: sueloImage.width,
+          y: canvas.height - sueloImage.height,
           image: sueloImage,
         }),
         new Platform({
-          x: sueloImage.width * 3 + 300,
-          y: 426,
+          x: sueloImage.width * 2,
+          y: canvas.height - sueloImage.height,
           image: sueloImage,
         }),
         new Platform({
-          x: sueloImage.width * 4 + 300 - 2,
-          y: 426,
+          x: sueloImage.width * 3,
+          y: canvas.height - sueloImage.height,
           image: sueloImage,
         }),
         new Platform({
-          x: sueloImage.width * 4 + 300 - 2,
-          y: 216,
-          image: createImage(brick1),
+          x: sueloImage.width * 4,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
         }),
         new Platform({
-          x: sueloImage.width * 5 + 700 - 2,
-          y: 226,
-          image: createImage(brick1),
+          x: sueloImage.width * 5,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
         }),
+        new Platform({
+          x: sueloImage.width * 6,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 7,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 8,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 9,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 10,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 11,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 12,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 13,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 14,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 15,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 16,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 17,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 18,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 19,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 20,
+          y: canvas.height - sueloImage.height,
+          image: sueloImage,
+        }),
+        new Platform({
+          x: sueloImage.width * 3,
+          y: canvas.height - sueloImage.height - brick1Image.height,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 5,
+          y: canvas.height - sueloImage.height - brick1Image.height * 2,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 7,
+          y: canvas.height - sueloImage.height - brick1Image.height,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 9,
+          y: canvas.height - sueloImage.height - brick1Image.height * 2,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 11,
+          y: canvas.height - sueloImage.height - brick1Image.height,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 13,
+          y: canvas.height - sueloImage.height - brick1Image.height,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 15,
+          y: canvas.height - sueloImage.height - brick1Image.height * 2,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 17,
+          y: canvas.height - sueloImage.height - brick1Image.height,
+          image: brick1Image,
+        }),
+        new Platform({
+          x: sueloImage.width * 19,
+          y: canvas.height - sueloImage.height - brick1Image.height * 2,
+          image: brick1Image,
+        }),
+      ];
+
+      enemies = [
+        new Enemy(
+          canvas.width,
+          canvas.height - sueloImage.height - goombaImage.height * 3,
+          createImage(goomba)
+        ),
       ];
 
       genericObjects = [
@@ -199,6 +355,64 @@ const SkillsGame = () => {
         }),
       ];
       scrollOfSet = 0;
+    };
+
+    const checkCollision = () => {
+      enemies.forEach((enemy, index) => {
+        if (
+          player.position.y < enemy.position.y + enemy.height &&
+          player.position.y + player.height > enemy.position.y &&
+          player.position.x + player.width > enemy.position.x &&
+          player.position.x < enemy.position.x + enemy.width
+        ) {
+          if (
+            player.velocity.y > 0 &&
+            player.position.y + player.height < enemy.position.y + 10
+          ) {
+            // El jugador ha colisionado en la cabeza del enemigo
+            enemies.splice(index, 1); // Elimina el enemigo del array
+          } else {
+            // El jugador ha colisionado con un enemigo, realiza las acciones correspondientes
+            // como reiniciar el juego o reducir la vida del jugador, etc.
+            init();
+          }
+        }
+      });
+    };
+
+    const updateBackground = () => {
+      backgroundOffset -= player.velocity.x; // Ajustamos el desplazamiento del fondo según la velocidad del jugador
+    };
+
+    const drawBackground = () => {
+      const { width, height } = genericObjects[0].image;
+      const canvasWidth = canvas.width;
+      const canvasHeight = canvas.height;
+
+      let offsetX = backgroundOffset % width;
+      if (offsetX < 0) {
+        offsetX += width;
+      }
+
+      for (let x = offsetX; x < canvasWidth; x += width) {
+        context.drawImage(genericObjects[0].image, x, canvasHeight - height);
+      }
+    };
+
+    const update = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      updateBackground();
+      drawBackground();
+
+      // Dibuja las plataformas y el jugador
+      platforms.forEach((platform) => {
+        platform.draw();
+      });
+
+      player.update();
+
+      requestAnimationFrame(update);
     };
 
     const animate = () => {
@@ -213,7 +427,13 @@ const SkillsGame = () => {
       platforms.forEach((platform) => {
         platform.draw();
       });
+      enemies.forEach((enemy) => {
+        enemy.update();
+      });
+
       player.update();
+
+      checkCollision();
 
       if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = player.speed;
@@ -257,9 +477,26 @@ const SkillsGame = () => {
         }
       });
 
-      //win condition
+      if (
+        currentKey === "right" &&
+        player.currentSprite !== player.sprites.run.right
+      ) {
+        player.frames = 1;
+        player.currentSprite = player.sprites.run.right;
+        player.currentCropWidth = player.sprites.run.cropWidth;
+        player.width = player.sprites.run.width;
+      } else if (
+        currentKey === "left" &&
+        player.currentSprite !== player.sprites.run.left
+      ) {
+        player.currentSprite = player.sprites.run.left;
+        player.currentCropWidth = player.sprites.run.cropWidth;
+        player.width = player.sprites.run.width;
+      }
+
       if (scrollOfSet > sueloImage.width * 4 + 700 - 2) {
-        console.log("you win");
+        //win condition
+        console.log("win");
       }
       //lose condition
       if (player.position.y > canvas.height) {
@@ -275,9 +512,7 @@ const SkillsGame = () => {
         case 65:
           console.log("left");
           keys.left.pressed = true;
-          player.currentSprite = player.sprites.run.left;
-          player.currentCropWidth = player.sprites.run.cropWidth;
-          player.width = player.sprites.run.width;
+          currentKey = "left";
           break;
         case 83:
           console.log("down");
@@ -285,9 +520,7 @@ const SkillsGame = () => {
         case 68:
           console.log("right");
           keys.right.pressed = true;
-          player.currentSprite = player.sprites.run.right;
-          player.currentCropWidth = player.sprites.run.cropWidth;
-          player.width = player.sprites.run.width;
+          currentKey = "right";
           break;
         case 87:
           console.log("up");
@@ -302,6 +535,7 @@ const SkillsGame = () => {
         case 65:
           console.log("left");
           keys.left.pressed = false;
+          currentKey = "left";
           break;
         case 83:
           console.log("down");
@@ -309,9 +543,7 @@ const SkillsGame = () => {
         case 68:
           console.log("right");
           keys.right.pressed = false;
-          player.currentSprite = player.sprites.stand.right;
-          player.currentCropWidth = player.sprites.stand.cropWidth;
-          player.width = player.sprites.stand.width;
+          currentKey = "right";
           break;
         case 87:
           console.log("up");
@@ -330,7 +562,6 @@ const SkillsGame = () => {
 
   return (
     <div className="lienzo">
-      <h1>Prueba de juego de habilidades</h1>
       <canvas className="juego" ref={canvasRef}></canvas>
     </div>
   );
