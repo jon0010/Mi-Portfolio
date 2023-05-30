4407c5b5f9374e77fd35d0448fb874b7142d40b1
Alan.-322 <99999496+jon0010@users.noreply.github.com>
2023-05-30 18:01:58 -0300

    testimonials finalizada

--- src/views/testimonials/Testimonials.jsx
+++ src/views/testimonials/Testimonials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import bowser from "../../assets/bowser.jpg";
-import spike from "../../assets/spike.jpg";
-import jumpman from "../../assets/jumpman.jpg";
+import alan from "../../assets/alan.jpg";
+import avatar from "../../assets/chachi.jpg";
+import chachi from "../../assets/avatar.jpg";
 import "./Testimonials.css";
 
 const Testimonials = () => {
@@ -27,21 +27,26 @@ const Testimonials = () => {
             <div className="container">
               <img
                 className="testimonio-imagen rounded-circle"
-                src={bowser}
-                alt="Foto de bowser"
+                src={alan}
+                alt="Foto de alan"
               />
               <p className="testimonio-texto">
-                "Estoy muy agradecido por el servicio que recibí de Medical
-                Connect. La plataforma de videollamadas es muy fácil de usar y
-                el médico que me atendió fue muy amable y eficiente. Me
-                diagnosticó rápidamente y me dio un tratamiento que me ayudó a
-                sentirme mejor. Definitivamente recomendaría esta aplicación a
-                cualquier persona que busque atención médica conveniente y
-                confiable".
+                "Tuve el privilegio de colaborar con jon y siempre fue un
+                compañero de equipo excepcional. Jon es un desarrollador
+                talentoso y versátil, capaz de trabajar en todas las capas de
+                una aplicación. Además, su actitud positiva, su disposición para
+                ayudar a otros y su habilidad para comunicarse eficazmente hacen
+                de jon un verdadero placer para trabajar. Sin duda alguna,
+                recomendaría a jon como un gran activo para cualquier equipo de
+                desarrollo."
               </p>
               <div className="testimonio-info">
-                <p className="cliente">Bowser</p>
-                <p className="cargo">Enemigo de mario x2</p>
+                <p className="cliente">
+                  Alan Abel Pereyra - Full Stack Developer
+                </p>
+                <p className="cargo">
+                  https://www.linkedin.com/in/alan-abel-pereyra-0a8324257/
+                </p>
               </div>
             </div>
           </div>
@@ -49,19 +54,24 @@ const Testimonials = () => {
             <div className="container">
               <img
                 className="testimonio-imagen rounded-circle"
-                src={jumpman}
-                alt="Foto de jumpman"
+                src={chachi}
+                alt="Foto de ibrahin"
               />
               <p className="testimonio-texto">
-                "Tuve una experiencia maravillosa con Medical Connect. Mi médico
-                me atendió enseguida y fue muy detallado en su explicación de mi
-                condición. La aplicación en sí es muy fácil de usar y estoy muy
-                contenta con la calidad del servicio que recibí. Definitivamente
-                volveré a usarla en el futuro".
+                "He tenido el placer de trabajar con jon y su contribución fue
+                realmente invaluable. Como Tech Lead, jon demostró una gran
+                capacidad para guiar al equipo. Su atención al detalle y su
+                habilidad para encontrar soluciones eficientes a problemas
+                complejos fueron fundamentales para el éxito. Trabajar con jon
+                fue una experiencia enriquecedora y definitivamente recomendaría
+                su experiencia y habilidades técnicas en cualquier entorno de
+                desarrollo."
               </p>
               <div className="testimonio-info">
-                <p className="cliente">jumpman</p>
-                <p className="cargo">mario antes de mario</p>
+                <p className="cliente">Dario Velazquez - Tech Lead en Prexto</p>
+                <p className="cargo">
+                  https://www.linkedin.com/in/velazquez-dario/
+                </p>
               </div>
             </div>
           </div>
@@ -69,20 +79,26 @@ const Testimonials = () => {
             <div className="container">
               <img
                 className="testimonio-imagen rounded-circle"
-                src={spike}
-                alt="Foto de spike"
+                src={avatar}
+                alt="Foto de dario"
               />
               <p className="testimonio-texto">
-                "Medical Connect me sorprendió con su eficiencia y facilidad de
-                uso. Nunca había tenido una consulta médica en línea, pero el
-                médico que me atendió fue muy amable y profesional. Me sentí muy
-                cómodo durante toda la consulta y me encantó no tener que salir
-                de mi casa. Definitivamente lo recomiendo a cualquier persona
-                que busque atención médica accesible y de calidad".
+                "Trabajar con jon fue una experiencia excepcional. Su capacidad
+                para comprender rápidamente los desafíos técnicos y proponer
+                soluciones innovadoras nos permitió alcanzar nuestros objetivos
+                en tiempo récord. Además, su habilidad para liderar al equipo y
+                mantener un ambiente de trabajo colaborativo fue fundamental
+                para el éxito del proyecto. Estoy impresionado por su
+                conocimiento técnico y su ética de trabajo impecable. Sin duda,
+                recomendaría a jon como un activo valioso en cualquier proyecto
+                técnico."
               </p>
               <div className="testimonio-info">
-                <p className="cliente">Spike</p>
-                <p className="cargo">Enemigo de mario</p>
+                <p className="cliente">Ibrahin Rodriguez - CTO en Docmovi</p>
+                <p className="cargo">
+                  {" "}
+                  https://www.linkedin.com/in/rodriguezibrahin/
+                </p>
               </div>
             </div>
           </div>
